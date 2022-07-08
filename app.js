f8a0ccda4c4f4cd65d8c74030a9761564311bfe2
Krishan Kumar <kkrishan7074@gmail.com>
2022-07-08 13:50:08 +0530

    Commit

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const click = document.getElementById("hamburger-lines");
 const show = document.getElementById("nav-list");
+const position = document.getElementById("social-info");
+const clickhere = document.getElementById("resume-header");
+
+show.style.display = "none";
+show.style.opacity = "0";
 
 click.addEventListener("click", () => {
-    show.style.opacity('0')
+  show.style.display = "block";
+  show.style.opacity = "1";
+  //   console.log('clicked');
 });
+
+if ((show.style.display = "block")) {
+  position.style.position = "relative";
+  position.style.top = "-55%";
+}
