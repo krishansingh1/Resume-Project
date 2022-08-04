c43f4497c0acc22bc6bd7b907ce62a59a415a134
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-04 23:14:40 +0530

    Commit

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
 const navbar = document.getElementById("hamburger-lines");
 const list = document.getElementById("nav-list");
 const display = document.getElementById("social-info");
-const about = document.getElementById("about");
-const skills = document.getElementById("skills");
-const experience = document.getElementById("experience");
-const education = document.getElementById("education");
-const portfolio = document.getElementById("portfolio");
-const contact = document.getElementById("contact");
+// const about = document.getElementById("about");
+// const skills = document.getElementById("skills");
+// const experience = document.getElementById("experience");
+// const education = document.getElementById("education");
+// const portfolio = document.getElementById("portfolio");
+// const contact = document.getElementById("contact");
 
 navbar.addEventListener("click", () => {
   list.classList.toggle("active");
   display.classList.toggle("active");
 });
 
-about.addEventListener("click", () => {
-  let timeRun = 0;
+// about.addEventListener("click", () => {
+//   let timeRun = 0;
 
-  let smoothScroll = setInterval(function () {
-    timeRun += 1;
-    if (timeRun === 2) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 20);
-});
+//   let smoothScroll = setInterval(function () {
+//     timeRun += 1;
+//     if (timeRun === 2) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 20);
+// });
 
-skills.addEventListener("click", () => {
-  let timeRun = 0;
-  let smoothScroll = setInterval(() => {
-    timeRun += 1;
-    if (timeRun === 13) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 20);
-});
+// skills.addEventListener("click", () => {
+//   let timeRun = 0;
+//   let smoothScroll = setInterval(() => {
+//     timeRun += 1;
+//     if (timeRun === 13) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 20);
+// });
 
-experience.addEventListener("click", () => {
-  let timeRun = 0;
-  let smoothScroll = setInterval(() => {
-    timeRun += 1;
-    if (timeRun == 24) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 20);
-});
+// experience.addEventListener("click", () => {
+//   let timeRun = 0;
+//   let smoothScroll = setInterval(() => {
+//     timeRun += 1;
+//     if (timeRun == 24) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 20);
+// });
 
-education.addEventListener("click", () => {
-  let timeRun = 0;
+// education.addEventListener("click", () => {
+//   let timeRun = 0;
 
-  let smoothScroll = setInterval(() => {
-    timeRun += 1;
-    if (timeRun === 43) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 20);
-});
+//   let smoothScroll = setInterval(() => {
+//     timeRun += 1;
+//     if (timeRun === 43) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 20);
+// });
 
-portfolio.addEventListener("click", () => {
-  let timeRun = 0;
-  let smoothScroll = setInterval(() => {
-    timeRun += 1;
-    if (timeRun === 58) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 10);
-});
+// portfolio.addEventListener("click", () => {
+//   let timeRun = 0;
+//   let smoothScroll = setInterval(() => {
+//     timeRun += 1;
+//     if (timeRun === 58) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 10);
+// });
 
-contact.addEventListener("click", () => {
-  let timeRun = 0;
-  let smoothScroll = setInterval(() => {
-    timeRun += 1;
-    if (timeRun === 75) {
-      clearInterval(smoothScroll);
-    }
-    window.scrollBy(0, 50);
-  }, 10);
-});
+// contact.addEventListener("click", () => {
+//   let timeRun = 0;
+//   let smoothScroll = setInterval(() => {
+//     timeRun += 1;
+//     if (timeRun === 75) {
+//       clearInterval(smoothScroll);
+//     }
+//     window.scrollBy(0, 50);
+//   }, 10);
+// });
