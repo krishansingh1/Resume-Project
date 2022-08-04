e854e06323d0133821f23cfdfe4a35f73c4cdeb0
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-04 14:12:05 +0530

    Commit

--- app.js
+++ app.js
@@ -4,24 +4,6 @@ const position = document.getElementById("social-info");
 const clickhere = document.getElementById("resume-header");
 const about = document.getElementById("about");
 
-var x = window.matchMedia("(max-width: 572px)");
-
-if (x.matches) {
-  show.style.display = "none";
-  show.style.opacity = "0";
-
-  click.addEventListener("click", () => {
-    show.style.display = "block";
-    show.style.opacity = "1";
-    //   console.log('clicked');
-  });
-
-  if ((show.style.display = "block")) {
-    position.style.position = "relative";
-    position.style.top = "-55%";
-  }
-}
-
 about.addEventListener("click", () => {
   let y = 0;
 
@@ -30,7 +12,7 @@ about.addEventListener("click", () => {
   }, 1000);
 
   if ((y = 200)) {
-    clearInterval(id);
+    smoothScroll(id);
   }
   // setTimeout(() => {
   //   window.scrollBy(0, 150);
