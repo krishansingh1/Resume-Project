e775e3cab46c59a038b5baf2557ef7b5532bf3cb
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-04 15:08:31 +0530

    Commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const navbar = document.getElementById("hamburger-lines");
 const list = document.getElementById("nav-list");
+const display = document.getElementById("social-info");
 const about = document.getElementById("about");
 
 navbar.addEventListener("click", () => {
   list.classList.toggle("active");
+  display.classList.toggle("active");
 });
 
 about.addEventListener("click", () => {
@@ -13,10 +15,7 @@ about.addEventListener("click", () => {
     window.scrollBy(0, (y = 200));
   }, 1000);
 
-  if ((y = 200)) {
-    smoothScroll(id);
-  }
-  // setTimeout(() => {
-  //   window.scrollBy(0, 150);
-  // }, 1000);
+  // if ((y = 200)) {
+  //   smoothScroll(id);
+  // }
 });
